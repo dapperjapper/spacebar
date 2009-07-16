4f1839075ba282bb3d753eb5fa265ded2161d954
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 14:04:18 +0000

    added space to delete links on sitemap

--- trunk/spacebar/system/sitemap.php
+++ trunk/spacebar/system/sitemap.php
@@ -11,7 +11,7 @@ $pages = get_all_pages($db);
 foreach ($pages as $page) {
   print '<li><a href="' . ROOT_DIR . '/' . $page['url'] . '" >' . ROOT_DIR . '/' . $page['url'] . '</a>';
   if (logged_in()) {
-    print '<a href="' . ROOT_DIR . '/' . $page['url'] . '/delete" >X</a>';
+    print ' <a href="' . ROOT_DIR . '/' . $page['url'] . '/delete" >X</a>';
   }
   print '</li>';
 }
