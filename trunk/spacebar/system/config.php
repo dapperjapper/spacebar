0a2ecfb8d6a2f8e6ba656e5f691e7c26cf61aa12
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 13:38:38 +0000

    another test

--- trunk/spacebar/system/config.php
+++ trunk/spacebar/system/config.php
@@ -3,6 +3,7 @@
 define("LOGIN_USERNAME", "admin");
 define("LOGIN_PASSWORD", "n3vrgu355");
 //Don't commit me!!
+//another test
 
-define("ROOT_DIR", "/spacebar"); //Begins with a "/"; the path to get from http://yoursite.com to the falling glass homepage. No slash on the end.
+define("ROOT_DIR", "/"); //Begins with a "/"; the path to get from http://yoursite.com to the falling glass homepage. No slash on the end.
 ?>
