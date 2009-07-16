b0368d653b4058fa04ec15d9632ee6a4083fbb98
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 13:15:09 +0000

    initial import

---
+++ spacebar/system/login.php
@@ -0,0 +1,37 @@
+<?php
+//login("blah", "hi");
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+  login($_POST['username'], $_POST['password']);
+  header("Location: login");
+} else {
+  if (logged_in()) {
+    if ($_POST['action'] == "logout") {
+      logout();
+      header("Location: .");
+    }
+?>
+<html>
+<head>
+</head>
+<body>
+<h1>Hello VIP.</h1>
+<p>Your highness is currently logged in. Would you like to <form action="login" method="post"><input type="hidden" name="action" value="logout" /><input type="submit" value="logout?" /></form></p>
+</body>
+</html>
+<?php
+  } else {
+?>
+<html>
+<head>
+</head>
+<body>
+<h1>Login, you not-logged-in person!!!</h1>
+<form action="login" method="post">
+<input name="username" value="your name, sire" />
+<input type="password" name="password" value="password" />
+<input type="submit" value="Try me" />
+</form>
+</body>
+</html>
+<?php }} ?>
