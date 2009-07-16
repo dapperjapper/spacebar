ea1454ef74427210e0525702d2130984eadbd4f9
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 19:14:10 +0000

    made edit button only appear to iPhone users

--- trunk/spacebar/system/templates/parts/header.php
+++ trunk/spacebar/system/templates/parts/header.php
@@ -1,7 +1,9 @@
 <div id="logo-outline" >
 <div id="logo-center" >
 <img src="<?=ROOT_DIR;?>/system/templates/parts/logo.png" />
+<?php if (preg_match("/iPhone/", $_SERVER['HTTP_USER_AGENT']) == 1) { ?>
 <a class="rounded_gray_button">edit</a>
+<?php } ?>
 </div>
 </div>
 <div id="container" >
