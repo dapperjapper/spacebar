dab0ad4ba957323c108ad637de2dee639e967aff
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 13:48:39 +0000

    changed config.php to config-sample.php

--- spacebar/system/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Please replace these values with your own!
-define("LOGIN_USERNAME", "admin");
-define("LOGIN_PASSWORD", "n3vrgu355");
-//Don't commit me!!
-//another test
-
-define("ROOT_DIR", "/"); //Begins with a "/"; the path to get from http://yoursite.com to the falling glass homepage. No slash on the end.
-?>
