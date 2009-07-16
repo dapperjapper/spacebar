5c5af0aae92299597986436556f9526f0a6a0609
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 19:07:11 +0000

    edit button

--- trunk/spacebar/system/templates/parts/header.php
+++ trunk/spacebar/system/templates/parts/header.php
@@ -1,6 +1,7 @@
 <div id="logo-outline" >
 <div id="logo-center" >
 <img src="<?=ROOT_DIR;?>/system/templates/parts/logo.png" />
+<a class="rounded_gray_button">edit</a>
 </div>
 </div>
 <div id="container" >
