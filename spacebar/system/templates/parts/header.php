693ad678d3a3a455bead8b6dad995557174b6510
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 19:35:25 +0000

    Made edit button only show if logged in, added javascript

--- spacebar/system/templates/parts/header.php
+++ spacebar/system/templates/parts/header.php
@@ -1,8 +1,8 @@
 <div id="logo-outline" >
 <div id="logo-center" >
 <img id="logo" src="<?=ROOT_DIR;?>/system/templates/parts/logo.png" />
-<?php if (preg_match("/iPhone/", $_SERVER['HTTP_USER_AGENT']) == 1) { ?>
-<a class="rounded_gray_button">edit</a>
+<?php if (preg_match("/iPhone/", $_SERVER['HTTP_USER_AGENT']) == 1 && logged_in()) { ?>
+<a class="rounded_gray_button" onclick="$('.editable').dblclick(); $('.editable-notextile').dblclick();">edit</a>
 <?php } ?>
 </div>
 </div>
