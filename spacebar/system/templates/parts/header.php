e87f6d9196f25fc6219314db0cb371f23865812b
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 19:30:20 +0000

    improved edit button

--- spacebar/system/templates/parts/header.php
+++ spacebar/system/templates/parts/header.php
@@ -1,6 +1,6 @@
 <div id="logo-outline" >
 <div id="logo-center" >
-<img src="<?=ROOT_DIR;?>/system/templates/parts/logo.png" />
+<img id="logo" src="<?=ROOT_DIR;?>/system/templates/parts/logo.png" />
 <?php if (preg_match("/iPhone/", $_SERVER['HTTP_USER_AGENT']) == 1) { ?>
 <a class="rounded_gray_button">edit</a>
 <?php } ?>
