c55147cc1dca65d3aa335e0b83cdcd0aae4c6a65
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-16 13:29:15 +0000

    testing ignore

--- spacebar/system/config.php
+++ spacebar/system/config.php
@@ -2,6 +2,7 @@
 //Please replace these values with your own!
 define("LOGIN_USERNAME", "admin");
 define("LOGIN_PASSWORD", "n3vrgu355");
+//Don't commit me!!
 
 define("ROOT_DIR", "/spacebar"); //Begins with a "/"; the path to get from http://yoursite.com to the falling glass homepage. No slash on the end.
 ?>
