bf612eb16d05edba0ee6a3e469769fb63f398483
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-17 13:49:54 +0000

    Deleting branch in favor of newer trunk

--- branches/spacebar.mobi/spacebar/system/config-sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Please replace these values with your own!
-define("LOGIN_USERNAME", "admin");
-define("LOGIN_PASSWORD", "adminpass");
-
-define("ROOT_DIR", "/spacebar"); //Begins with a "/"; the path to get from http://yoursite.com to the falling glass homepage. No slash on the end.
-?>
--- branches/spacebar.mobi/spacebar/system/notfound.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-<h1>The 'ol 404</h1>
-<p>This page does not exist in our known universe.</p>
-</body>
-</html>
