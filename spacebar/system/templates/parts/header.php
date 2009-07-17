0960b645e2d58ae04497bcf654ed251ffd04f2ca
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-17 14:15:08 +0000

    Merged back into r27, removing all the stuff that was getting into the branch, spacebar.mobi

--- spacebar/system/templates/parts/header.php
+++ spacebar/system/templates/parts/header.php
@@ -1,16 +1,9 @@
 <div id="logo-outline" >
 <div id="logo-center" >
-<a href="<?=ROOT_DIR;?>"><img id="logo" src="<?=ROOT_DIR;?>/system/templates/parts/logo.png" /></a>
-<?php if (logged_in()) { ?>
-<a class="rounded_gray_button" >logout</a>
+<img id="logo" src="<?=ROOT_DIR;?>/system/templates/parts/logo.png" />
+<?php if (preg_match("/iPhone/", $_SERVER['HTTP_USER_AGENT']) == 1) { ?>
+<a class="rounded_gray_button">edit</a>
 <?php } ?>
 </div>
 </div>
-
-<ul id="breadcrumbs">
-  <li><a href="#">Susan Smith</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" /></li>
-  <li><a href="#">Poems</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" />
-  <li><a id="select-crumb" href="#">Squirrel</a><img src="<?=ROOT_DIR;?>/system/templates/parts/outline-breadcrumbs-divider.png" /></li>
-</ul>
-
 <div id="container" >
