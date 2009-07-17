f55fef814bda5d035212c78795c488cb1b8a82a1
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-17 03:52:57 +0000

    breadcrumbs images

--- trunk/spacebar/system/templates/parts/header.php
+++ trunk/spacebar/system/templates/parts/header.php
@@ -6,4 +6,7 @@
 <?php } ?>
 </div>
 </div>
+<div id="breadcrumbs">
+  <a href="#">Susan Smith</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" /><a href="#">Poems</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" /><a href="#">Squirrel</a><img src="<?=ROOT_DIR;?>/system/templates/parts/select-end.png" /><a id="select-crumb" href="#">Select</a><img src="<?=ROOT_DIR;?>/system/templates/parts/select-tip.png" />
+</div>
 <div id="container" >
