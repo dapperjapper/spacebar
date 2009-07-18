7461ef8eb2c119fa6be87ba721f577d273ca623c
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-18 17:59:03 +0000

    urgg. r55 didn't actually commit all the changes. here they are

--- spacebar/system/templates/blog.php
+++ spacebar/system/templates/blog.php
@@ -1,11 +1,11 @@
 <?php
-$subpages = get_subpages($db, $url);
-foreach ($subpages as $key => $subpage) {
-  $subpages[$key]['blocks']['title'] = get_block_data($db, $subpage['id'], "title", false, "Unnamed Post");
-  $subpages[$key]['blocks']['datepub'] = get_block_data($db, $subpage['id'], "datepub", false, date("D M j Y"));
-  $subpages[$key]['blocks']['tspub'] = strtotime($subpages[$key]['blocks']['datepub']);
+$blogsubpages = get_subpages($db, $url);
+foreach ($blogsubpages as $key => $blogsubpage) {
+  $blogsubpages[$key]['blocks']['title'] = get_block_data($db, $blogsubpage['id'], "title", false, "Unnamed Post");
+  $blogsubpages[$key]['blocks']['datepub'] = get_block_data($db, $blogsubpage['id'], "datepub", false, date("D M j Y"));
+  $blogsubpages[$key]['blocks']['tspub'] = strtotime($blogsubpages[$key]['blocks']['datepub']);
 }
-usort($subpages, "sort_by_date");
+usort($blogsubpages, "sort_by_date");
 function sort_by_date ($a, $b) {
   return ($a['blocks']['tspub'] < $b['blocks']['tspub']) ? 1 : -1;
 }
@@ -23,8 +23,8 @@ $title = get_block_data($db, $page['id'], "title", false);
 <?=block($db, $page['id'], "title", "h1", false, $title);?>
 <ul>
 <?php
-foreach ($subpages as $subpage) {
-  print '<li><a href="' . ROOT_DIR . '/' . $subpage['url'] . '" >' . $subpage['blocks']['title'] . ' (' . $subpage['blocks']['datepub'] . ')</a></li>';
+foreach ($blogsubpages as $blogsubpage) {
+  print '<li><a href="' . ROOT_DIR . '/' . $blogsubpage['url'] . '" >' . $blogsubpage['blocks']['title'] . ' (' . $blogsubpage['blocks']['datepub'] . ')</a></li>';
 }
 ?>
 </ul>
--- spacebar/system/templates/parts/header.php
+++ spacebar/system/templates/parts/header.php
@@ -12,16 +12,36 @@
 </div>
 
 <ul id="breadcrumbs">
-  <li><a href="#">Susan Smith</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" /></li>
-  <li><a href="#">Poems</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" />
-  <li><a href="#">My Animal Series</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" />
-  <li><a href="#">Mammals</a><img src="<?=ROOT_DIR;?>/system/templates/parts/breadcrumbs-divider.png" />
-  <li><a id="select-crumb" href="#">Squirrel</a><img src="<?=ROOT_DIR;?>/system/templates/parts/outline-breadcrumbs-divider.png" />
-  <select id="select" onchange="getElementById('select-crumb').innerHTML=this.value" style="margin-left: -85px; opacity: 0;" >
-    <option>Lion</option>
-    <option>Squirrel</option>
-  </select>
-  </li>
+<?php
+$tempsplitpath = $splitpath;
+if ($splitpath != array("")) {
+  array_unshift($tempsplitpath, "");
+}
+foreach ($tempsplitpath as $key => $crumb) {
+  $crumbsplitpath = array_slice($splitpath, 0, $key);
+  $crumburl = implode("/", $crumbsplitpath);
+  $crumbpage = get_page_by_url($db, $crumburl);
+  $crumbtitle = get_block_data($db, $crumbpage['id'], "title", false);
+  echo '<li><a href="' . ROOT_DIR . '/' . $crumburl . '">' . $crumbtitle . '</a><img src="' . ROOT_DIR . '/system/templates/parts/breadcrumbs-divider.png" /></li>';
+}
+?>
+<li><a id="select-crumb" href="#">Select...</a><img src="<?=ROOT_DIR;?>/system/templates/parts/outline-breadcrumbs-divider.png" />
+<select id="select" onchange="if (this.value=='new') { $('#select-crumb').html('New...'); $('#newpagespan').show(); } else if (this.value=='select') { $('#select-crumb').html('Select...'); $('#newpagespan').hide(); } else { window.location='<?=ROOT_DIR;?>/'+this.value; }" style="position: fixed; opacity: 0; margin-top: 6px; margin-left: -105px;" >
+<option value="select" selected >Select...</option>
+<option value="new" >New...</option>
+<?php
+$subpages = get_subpages($db, $page['url']);
+foreach ($subpages as $subpage) {
+  $pagetitle = get_block_data($db, $subpage['id'], "title", false);
+  echo '<option value="' . $subpage['url'] . '" >' . $pagetitle . '</option>';
+}
+?>
+</select>
+<span style="display: none;" id="newpagespan" >
+<input style="margin-left: 20px;" id="newpagetextfield" />
+<button onclick="window.location = '<?=ROOT_DIR;?>/<?php if ($url!="") { echo $url . "/"; }?>'+$('#newpagetextfield').val();" >Go</button>
+</span>
+</li>
 </ul>
 
 <div id="container" >
