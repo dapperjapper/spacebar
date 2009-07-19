025cbb4461c24a57660c006f39a6aa6464f886d8
lclarkberg <lclarkberg@0ce2d318-6ff3-11de-9e9f-05296fd93cd0>
2009-07-19 00:42:38 +0000

    made deletepage show full url before deleting

--- trunk/spacebar/system/deletepage.php
+++ trunk/spacebar/system/deletepage.php
@@ -1,17 +1,17 @@
 <?php
 if (logged_in()) {
-	$splitpath = array_slice($splitpath, 0, -1);
+  $splitpath = array_slice($splitpath, 0, -1);
   $name = $splitpath[count($splitpath)-1];
   $url = implode('/', $splitpath);
 } else {
-	die("Bad boy/girl! Login first!");
+  die("Bad boy/girl! Login first!");
 }
 ?>
 <html>
 <head>
 </head>
 <body>
-<h1>Are you sure you want to delete page "<?php if ($name == "") {print "index";} else {print $name;} ?>" and all the content on it?</h1>
+<h1>Are you sure you want to delete page "<?php if ($url == "") {print "index";} else {print $url;} ?>" and all the content on it?</h1>
 <a href="<?=ROOT_DIR?>/<?=$url?>" >no</a> or <a href="<?=ROOT_DIR?>/<?=$url?>/reallydelete" >yes</a>
 </body>
 </html>
